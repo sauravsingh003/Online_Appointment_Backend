5f59212439cd7143423a66857d1be71747b23e25
Saurav Singh <sauravchauhan0017@gmail.com>
2025-07-31 11:45:43 +0530

    Latest Commit

---
+++ src/main/java/com/app/Service/AppointmentService.java
@@ -0,0 +1,25 @@
+package com.app.Service;
+
+import java.util.List;
+
+import com.app.DTO.AppointmentDTO;
+import com.app.DTO.UpdateAppointmentStatusDTO;
+import com.app.Entity.Appointment;
+
+public interface AppointmentService {
+
+	// Appointment bookAppointment(BookAppointmentDTO dto);
+
+	// List<Patient> getAppointmentsByDoctor(Long doctorId);
+
+	Appointment updateAppointmentStatus(UpdateAppointmentStatusDTO dto);
+
+	// List<AppointmentResponseDTO> getAppointmentsByUserId(Long userId);
+
+	void bookAppointment(AppointmentDTO appointmentDTO);
+
+	List<Appointment> findByPatientId(Long patientId);
+
+	List<Appointment> getAppointmentsByDoctor(Long doctorId);
+
+}
